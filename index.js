c4f91cdcc636c5ab067db54f0f4fc35d3625443c
NatanBR836 <you@example.com>
2022-04-07 18:08:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var texto1 = document.getElementById("b1");
+var email = document.getElementById("b2");
+
+//metodo para mostrar um alerta de treinamento
+function Vaicaminhao() {
+     alert("Formulário enviado com sucesso!" + texto1.value + "" + email.value);
+}
+
